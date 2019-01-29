f34f8913da4981f95fcb50f1bd024fde807a9a4a
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-01-28 18:55:29 -0600

    completed food api with understanding

--- exercises/food-api/src/scripts/foodapi.js
+++ exercises/food-api/src/scripts/foodapi.js
@@ -1,32 +1,58 @@
 fetch("http://localhost:8088/foods")
-    .then(foods => foods.json())
-    .then(parsedFoods => {
-        console.table(parsedFoods)
-        loopper(parsedFoods) //calls the loopper fucntion and pass in the foods//
+    .then(response => response.json())
+    .then(myParsedFoods => {
+        myParsedFoods.forEach(food => {
+            console.log(food) // Should have a `barcode` property
+
+            // Now fetch the food from the Food API
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                    console.log(productInfo)
+                    // food.ingredients = productInfo.product.ingredients.map(i => {
+                    //     return `<li>${i.text}</li>`
+                    // }).join("")
+                    food.ingredients = productInfo.product.ingredients.reduce((a, c) => {
+                      return `${a}<li>${c.text}</li>`
+                    },"")
+                    
+                    food.country = productInfo.product.countries
+                    productInfo.product.nutriments.energy_serving
+                    food.fat = productInfo.product.nutriments.fat_serving
+                    food.sugars = productInfo.product.nutriments.sugars_serving
+                    
+                    // Produce HTML representation...the food argument is past in from the forEach statement at the top
+                    const foodAsHTML = foodFactory(food)
+
+                    // Add representaiton to DOM
+                    addFoodToDom(foodAsHTML)
+                })
+        })
     })
-//Make an HTML component for each food
-const createFoodHTML = food => `
-   <section class="foodListOne">
-   <header class="foodName">
-     <h1>${food.name}</h1>
-   </header>
 
-   <p class="foodCategory">
-     ${food.category}
-   </p>
-
-   <p class="foodEthnicity">
-   ${food.ethnicity}
-   </p>  
-   </div>    
-`
-//Put component on DOM...you can also seperate this loop
-function loopper(foods) {
-    for (let food of foods) {
-        const theFoodHTML = createFoodHTML(food)
-
-        const storageContainer = document.querySelector(".foodList")
-
-        storageContainer.innerHTML += theFoodHTML
+//function to create HTML template
+    const foodFactory = (food) => {
+        return `
+                <div class="food__items">
+                <h3>${food.name}</h3>
+                <h4>Ethnicity</h4>
+                <div class="descriptions ethnicity">${food.ethnicity}</div>
+                <h4>Category</h4>
+                <div class="descriptions category">${food.category}</div>
+                <h4>Ingredients</h4>
+                <div class="descriptions ingredients">${food.ingredients}</div>
+                <h4>Country</h4>
+                <div class="descriptions country">${food.country}</div>
+                <h4>Calories Per Serving</h4>
+                <div class="descriptions calories">${food.calories}</div>
+                <h4>Fat Per Serving</h4>
+                <div class="descriptions fat">${food.fat}</div>
+                <h4>Sugars Per Serving</h4>
+                <div class="descriptions sugar">${food.sugars}</div>
+    </div>
+                `
     }
-}
+//function to add the 
+    const addFoodToDom = (foodAsHTML) => {
+        document.querySelector(".foodList").innerHTML += foodAsHTML
+    }
